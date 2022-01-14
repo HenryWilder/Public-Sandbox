e61e5b06ae07885eec3e60e2de84918de1a6186e
Amy <henrythepony@gmail.com>
2022-01-13 20:07:32 -0500

    Made graphic progress

--- CChess3/CChess3/CChess3.cpp
+++ CChess3/CChess3/CChess3.cpp
@@ -1,20 +1,161 @@
-// CChess3.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+#include <Windows.h>
+#include <windowsx.h>
+#include <vector>
+#include <fstream>
 
-#include <iostream>
+HDC g_hdc;
+
+float Lerp(float a, float b, float t)
+{
+    return a + t * (b - a);
+}
+
+struct Animation
+{
+    Animation()
+    {
+        end = 0.0f;
+        Generate();
+    }
+
+    float start, control, end;
+    static float t;
+
+    static void Tick()
+    {
+        constexpr float tick = (1.0f / 120.0f);
+        t += tick;
+        if (t >= 1.0f)
+            t = 0.0f;
+    }
+    void Generate()
+    {
+        start = end;
+        control = 3.0f * ((float)(rand() - (RAND_MAX / 2)) / RAND_MAX);
+        end = 3.0f * ((float)(rand() - (RAND_MAX / 2)) / RAND_MAX);
+    }
+    float Sample() const
+    {
+        return Lerp(Lerp(start, control, t), Lerp(control, end, t), t);
+    }
+};
+float Animation::t = 0.0f;
+
+struct Triangle
+{
+    Triangle(std::ifstream& stream)
+    {
+        {
+            int r, g, b;
+            stream >> r >> g >> b;
+            color = RGB(r, g, b);
+        }
+        for (int i = 0; i < 3; ++i)
+        {
+            stream >> apt[i].x >> apt[i].y;
+        }
+    }
+    Triangle(
+        int r, int g, int b,
+        LONG x0, LONG y0,
+        LONG x1, LONG y1,
+        LONG x2, LONG y2)
+    {
+        color = RGB(r,g,b);
+        apt[0].x = x0;
+        apt[0].y = y0;
+        apt[1].x = x1;
+        apt[1].y = y1;
+        apt[2].x = x2;
+        apt[2].y = y2;
+    }
+
+    COLORREF color;
+    POINT apt[3];
+    Animation ptAnim[3][2];
+
+    void Anim_Generate()
+    {
+        for (unsigned int i = 0; i < 3; ++i)
+        {
+            for (unsigned int j = 0; j < 2; ++j)
+            {
+                ptAnim[i][j].Generate();
+            }
+        }
+    }
+};
+struct VectorGraphic
+{
+    VectorGraphic(const char* filename)
+    {
+        std::ifstream file(filename);
+
+        unsigned int count;
+        file >> count;
+        for (unsigned int i = 0; i < count; ++i)
+        {
+            paths.emplace_back(file);
+        }
+
+        file.close();
+    }
+
+    std::vector<Triangle> paths;
+
+    void Anim_Generate()
+    {
+        for (Triangle& tri : paths)
+        {
+            tri.Anim_Generate();
+        }
+    }
+};
+
+void DrawVectorGraphic(const VectorGraphic& vg, LONG x, LONG y)
+{
+    for (const Triangle& tri : vg.paths)
+    {
+        HBRUSH hBrush = CreateSolidBrush(tri.color);
+        SelectObject(g_hdc, hBrush);
+        POINT apt[3];
+        for (int i = 0; i < 3; ++i)
+        {
+            apt[i].x = tri.apt[i].x + x + (LONG)(tri.ptAnim[i][0].Sample() + 0.5f);
+            apt[i].y = tri.apt[i].y + y + (LONG)(tri.ptAnim[i][1].Sample() + 0.5f);
+        }
+        Polygon(g_hdc, apt, 3);
+        DeleteObject(hBrush);
+    }
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    HWND hWnd = GetConsoleWindow();
+    g_hdc = GetDC(hWnd);
+
+    HPEN hPen = CreatePen(PS_NULL, 0, RGB(0, 0, 0));
+    SelectObject(g_hdc, hPen);
+
+    VectorGraphic pawn("pawn");
+
+    for (int i = 0; i < 800; ++i)
+    {
+        DrawVectorGraphic(pawn, 100, 100);
+
+        Sleep(8);
+
+        Animation::Tick();
+        if (Animation::t == 0.0f)
+            pawn.Anim_Generate();
+    }
+
+    DeleteObject(hPen);
+
+    GetStockObject(WHITE_BRUSH);
+    GetStockObject(DC_PEN);
+
+    ReleaseDC(hWnd, g_hdc);
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file

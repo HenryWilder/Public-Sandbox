c9095e6dd6923738b74c59c6febc9814b95a5508
Amy <henrythepony@gmail.com>
2022-02-19 09:23:15 -0500

    Starting

--- GlooGraph/GlooGraph/GlooGraph.cpp
+++ GlooGraph/GlooGraph/GlooGraph.cpp
@@ -1,6 +1,31 @@
-#include <iostream>
+struct Vector3
+{
+    float x, y, z;
+};
+
+struct Transform
+{
+    Vector3 position;
+    Vector3 rotation;
+    Vector3 scale;
+};
+
+class Actor
+{
+    Transform transform;
+};
+
+class GlooProjectile : public Actor
+{
+
+};
+
+class Gloo : public Actor
+{
+    Actor* parent;
+};
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    return 0;
 }
